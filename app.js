f1428c8da1690638459145458d2687b34ddeabd9
dmaharjan <dilipmaharjann@gmail.com>
2017-09-23 17:52:16 -0400

    added and configured express

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Node");
+const express = require('express');
+const app = express();
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Hello Node');
+});
+
+//listener
+app.listen(3000, () => {
+    console.log('Server started at port 3000 ...')
+});
